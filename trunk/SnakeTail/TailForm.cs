a363c2b02dc262a88b08569176303b2934ef2b84
sweaty1 <sweaty1@094deec3-d956-94f0-060e-14986379faf8>
2011-09-20 20:55:34 +0000

    Fixed performance bug when Performance Counters are not available for monitoring a process

--- trunk/SnakeTail/TailForm.cs
+++ trunk/SnakeTail/TailForm.cs
@@ -250,13 +250,16 @@ namespace SnakeTail
             {
                 try
                 {
-                    double cpuUtilization = _taskMonitor.ProcessorUsage;
+                    float cpuUtilization = _taskMonitor.ProcessorUsage;
                     Process process = _taskMonitor.Process;
                     if (process != null)
                     {
-                        TimeSpan cpuTime = DateTime.Now.Subtract(_lastFormTitleUpdate);
-                        cpuUtilization = cpuUtilization / cpuTime.TotalMilliseconds * 1000.0;
-                        title += " CPU: " + cpuUtilization.ToString("F0");
+                        if (!float.IsNaN(cpuUtilization))
+                        {
+                            TimeSpan cpuTime = DateTime.Now.Subtract(_lastFormTitleUpdate);
+                            double cpuUsage = (double)cpuUtilization / cpuTime.TotalMilliseconds * 1000.0;
+                            title += " CPU: " + cpuUtilization.ToString("F0");
+                        }
                         process.Refresh();
                         title += " RAM: " + (process.PrivateMemorySize64 / (1024 * 1024)).ToString();
                         title += _taskMonitor.ServiceRunning ? " (Started)" : " (Stopped)";
