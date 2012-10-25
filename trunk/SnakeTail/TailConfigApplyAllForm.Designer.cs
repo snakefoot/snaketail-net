8df197fd82fb98096ca62dcb239515b25ad1e42c
sweaty1 <sweaty1@094deec3-d956-94f0-060e-14986379faf8>
2012-10-25 22:02:16 +0000

    Removed minimize / maximize buttons from popup dialogs

--- trunk/SnakeTail/TailConfigApplyAllForm.Designer.cs
+++ trunk/SnakeTail/TailConfigApplyAllForm.Designer.cs
@@ -1,4 +1,19 @@
-﻿namespace SnakeTail
+﻿#region License statement
+/* SnakeTail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+namespace SnakeTail
 {
     partial class TailConfigApplyAllForm
     {
@@ -100,7 +115,10 @@
             this.Controls.Add(this._checkBoxFont);
             this.Controls.Add(this._checkBoxColors);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "TailConfigApplyAllForm";
+            this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Apply View Options to All";
