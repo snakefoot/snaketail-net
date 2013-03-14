f89973a73ee0f4fe2fa7de86017c65cb4769b53a
sweaty1 <sweaty1@094deec3-d956-94f0-060e-14986379faf8>
2013-03-14 21:41:53 +0000

    Fixed crash when the Clipboard has been locked by another application

---
+++ trunk/SnakeTail/ClipboardHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SnakeTail
+{
+    sealed internal class ClipboardHelper
+    {
+        public static void CopyToClipboard(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+                return;
+
+            try
+            {
+                Clipboard.SetText(content);
+            }
+            catch (Exception firstException)
+            {
+                System.Diagnostics.Debug.WriteLine("Clipboard cannot be updated, maybe locked by another application: " + firstException.Message);
+                try
+                {
+                    // Perform a last retry as recommended on stackoverflow
+                    System.Threading.Thread.Sleep(100);
+                    Clipboard.Clear();
+                    System.Threading.Thread.Sleep(100);
+                    Clipboard.SetDataObject(content, true, 0, 0);
+                }
+                catch (Exception retryException)
+                {
+                    System.Diagnostics.Debug.WriteLine("Clipboard cannot be updated, maybe locked by another application: " + retryException.Message);
+                    throw firstException;
+                }
+            }
+        }
+    }
+}
