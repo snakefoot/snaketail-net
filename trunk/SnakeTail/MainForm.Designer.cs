6a697a2178b98e4a6cfa2393377e0a41108ef32c
sweaty1 <sweaty1@094deec3-d956-94f0-060e-14986379faf8>
2014-12-01 21:56:39 +0000

    Issue #77 - File menu keyboard shortcuts (ALT + F4)

--- trunk/SnakeTail/MainForm.Designer.cs
+++ trunk/SnakeTail/MainForm.Designer.cs
@@ -243,6 +243,7 @@ namespace SnakeTail
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
