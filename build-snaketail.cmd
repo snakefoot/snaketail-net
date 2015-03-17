d970e5fc633abc57f2df6cff65514edf8cbc99d6
Snakefoot <sweaty1@hotmail.com>
2015-03-18 00:48:40 +0100

    Version 1.9.1

--- build-snaketail.cmd
+++ build-snaketail.cmd
@@ -1,4 +1,4 @@
 @echo off
-msbuild SnakeTail\SnakeTail.csproj /p:Configuration=Release
+msbuild SnakeTail\SnakeTail.csproj /p:Configuration=Release /t:Clean;Rebuild
 msbuild SnakeTail.Wix\SnakeTail.Wix.wixproj /p:Configuration=Release;Platform=x86
 msbuild SnakeTail.Wix\SnakeTail.Wix.wixproj /p:Configuration=Release;Platform=x64
