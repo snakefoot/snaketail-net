7a75607ae619b4dd282d85f3a430bbb79823ddea
Rolf Kristensen <sweaty1@hotmail.com>
2016-08-20 17:23:17 +0200

    Removed System.Data reference, as it was not needed

--- SnakeTail/TailForm.cs
+++ SnakeTail/TailForm.cs
@@ -16,12 +16,10 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
-using System.Collections;
 using System.Diagnostics;
 
 namespace SnakeTail
--- SnakeTail/ThreadExceptionDialogEx.cs
+++ SnakeTail/ThreadExceptionDialogEx.cs
@@ -16,7 +16,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Net;
 using System.Net.Mail;
